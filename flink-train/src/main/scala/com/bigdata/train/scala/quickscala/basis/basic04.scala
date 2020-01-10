addcaf6ab94cece150a83ed9fd377e37b9176ed3
realguoshuai <764143033@qq.com>
2020-01-10 10:42:12 +0800

    Scala Junit 测试字符串插值器s

--- flink-train/src/main/scala/com/bigdata/train/scala/quickscala/basis/basic04.scala
+++ flink-train/src/main/scala/com/bigdata/train/scala/quickscala/basis/basic04.scala
@@ -1,18 +1,27 @@
 package com.bigdata.train.scala.quickscala.basis
 
+import org.junit.{Rule, Test}
+import org.junit.rules.TemporaryFolder
+
 /**
-  * Description 
+  * Description  字符串插值器
   * Created with guoshuai 
   * date 2020/1/6 11:35
   **/
-object basic04 {
+class basic04 {
+    @Rule
+    def tempFolder: TemporaryFolder = new TemporaryFolder()
 
     //字符串插值器 's'
-
-
-    def main(args: Array[String]): Unit = {
-
-
+    @Test
+    def testS(): Unit = {
+        val i = 1234
+        println(s"${i}")
     }
 
+    @Test
+    def testS1():Unit ={
+        val i = 1234
+        println(s"${i} 123")
+    }
 }
