bc1b0835da7316f09f29d545eab6db17e8c6b72e
realguoshuai <764143033@qq.com>
2020-01-10 10:11:21 +0800

    Scala Junit 单元测试

---
+++ flink-train/src/main/scala/com/gs/train/scala/junit/JunitTest.scala
@@ -0,0 +1,27 @@
+package com.gs.train.scala.junit
+
+import org.junit.{Rule, Test}
+import org.junit.rules.TemporaryFolder
+
+/**
+  * Description Scala单元测试
+  * Created with guoshuai 
+  * date 2020/1/10 10:07
+  **/
+class JunitTest {
+    //构建Junit
+    val _tempFolder = new TemporaryFolder
+
+    /**
+      * Description @Rule 构造测试用具（fixture）时初始化方法和清理方法的替代和补充
+      *
+      * return org.junit.rules.TemporaryFolder
+      **/
+    @Rule
+    def tempFolder: TemporaryFolder = _tempFolder
+
+    @Test
+    def testJunit(): Unit = {
+        println("junit test")
+    }
+}
